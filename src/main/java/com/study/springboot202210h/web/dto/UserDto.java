69898a8c0be8c922be18373e9bf4292b31aa0e6b
1nyeong <inyeong0726@gmail.com>
2022-12-16 21:55:40 +0900

    12.16

---
+++ src/main/java/com/study/springboot202210h/web/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.study.springboot202210h.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDto {
+    private int userId;
+    private String userName;
+    private String password;
+    private String name;
+    private String email;
+}
